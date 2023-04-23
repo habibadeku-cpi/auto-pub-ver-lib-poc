fe0e35f2d90385970c2c19beedf5a2c690a9247b
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-23 20:02:04 +0200

    fix(test): prepare for merge feature to main

--- lib/hello-app/HelloApp.tsx
+++ lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - Beta Branch (Have fun!!!)</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - New Feature Release to Production</div>;
   
 };
