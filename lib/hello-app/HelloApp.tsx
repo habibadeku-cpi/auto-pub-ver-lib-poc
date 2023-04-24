ceb4735d5ba49d47d350dfb06eca331ee36fa023
Habib Adeku <habib.adeku@chargepoint.com>
2023-04-24 11:21:46 +0200

    feat(update): update message

--- lib/hello-app/HelloApp.tsx
+++ lib/hello-app/HelloApp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 export const HelloApp = (props: any) => {
 
-    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - Some New Feature under Development</div>;
+    return <div style={{fontSize:'40px', textAlign: 'center', fontWeight: 'bolder', color: 'cadetblue'}}>Hello From Auto Published Lib - Update Message For Production Release</div>;
   
 };
